9be47b5c219f25501a824c244aee03240be61c4b
Dinkus <robertsnaer901@gmail.com>
2019-09-05 09:50:10 +0000

    Made changes to code

--- maximum_number.py
+++ maximum_number.py
@@ -1,10 +1,9 @@
 running = True
 max_int = 0
 while running: # While running is not False
-    user_in = int(input("Input a number: "))
-    if user_in < 0:
+    num_int = int(input("Input a number: "))
+    if num_int < 0:
         running = False
-    elif user_in%2==0: # if user input is an even number and not a negative number
-        if user_in > max_int:    # If user input is bigger than current maximum even number
-            max_int = user_in
+    elif num_int > max_int:    # If user input is bigger than current maximum even number
+        max_int = num_int
 print("The maximum is", max_int)
